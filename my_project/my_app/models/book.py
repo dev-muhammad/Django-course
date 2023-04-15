ed2ee0304d5a102006ab201ef2eca45eca40c2c2
Muhammad <63408454+dev-muhammad@users.noreply.github.com>
2023-04-15 13:32:09 +0500

    lesson 5 code

---
+++ my_project/my_app/models/book.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Book(models.Model):
+
+    title = models.CharField("Название", max_length=120)
+    author = models.CharField("Автор", max_length=120)
+    cover_type = models.CharField("Тип переплета", 
+                                  max_length=25,
+                                  default="Paper",
+                                  )
+    publish_year = models.IntegerField("Год публикации", 
+                                       blank=True, null=True)
+    create_time = models.DateTimeField("Дата создания", 
+                                       auto_now_add=True)
+    update_time = models.DateTimeField("Дата обновления",
+                                       auto_now=True)
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = "Книга"
+        verbose_name_plural = "Книги"
+
+    def __str__(self) -> str:
+        return self.title
+      
