553a36cc6f6695113dfbd6ad1b27368a9e6b8bf6
Muhammad <63408454+dev-muhammad@users.noreply.github.com>
2023-06-03 07:33:01 +0500

    docker and scripts

---
+++ docker and scripts/scripts/django.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+python manage.py loaddata fixtures/*
+
+python -u manage.py runserver 0.0.0.0:8000
